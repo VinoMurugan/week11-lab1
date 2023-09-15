5494506e41e8399b4cf4cba88fc5b190368d08f9
VinoMurugan <vinothini.jit@gmail.com>
2023-09-15 14:43:35 -0400

    greeting

---
+++ server.js
@@ -0,0 +1,20 @@
+
+// Greeting
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/greeting', (req, res) => {
+  res.send('Hello, stranger');
+});
+
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Wow! Hello there, ${name}!`);
+  });
+  
+  app.listen(port, () => {
+    console.log('listening on port' , port);
+  });
+
